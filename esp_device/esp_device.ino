1c5dd5b7fc1b852ed53f4e76aeb0d7a23bde9280
Kirill Mikeylov <keria132@protonmail.com>
2022-08-23 03:44:14 +0300

    - Added "relay switch" mechanic

--- esp_device/esp_device.ino
+++ esp_device/esp_device.ino
@@ -1,12 +1,17 @@
 /*Code for ESP-01, which transfers data from DHT sensor to the main server via WiFi connection.*/
 
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
 #include "DHT.h"
 int id = 0; //to identify from which esp data will be received
+int ipId = 100 + id + 1;
+short firstRelayPin = 5; //GPIO5 = D1 PIN
+short secondRelayPin = 4;  //GPI04 = D2 PIN
+unsigned long timer;
 
 // Set your Static IP address
-IPAddress local_IP(192, 168, 0, 101);
+IPAddress local_IP(192, 168, 0, ipId);
 // Set your Gateway IP address
 IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
@@ -19,17 +24,18 @@ float Temperature;
 float Humidity;
 
 //SSID and Password to connect to wifi
-const char *ssid = "ssid";
-const char *password = "password";
+const char *ssid = "Tobik_Hata";
+const char *password = "P4npYfYS";
 
-ESP8266WebServer server(80);
+AsyncWebServer server(80);
 
 void setup() {
 
   delay(1000);
   Serial.begin(115200);
 
-  pinMode(lightsPin, OUTPUT);
+  pinMode(firstRelayPin, OUTPUT);
+  pinMode(secondRelayPin, OUTPUT);
   
   pinMode(DHTPin, INPUT);
   dht.begin();
@@ -54,12 +60,19 @@ void setup() {
 }
 
 void loop() {
-//  
+
+  if(millis()-timer >= 3000){
+    sendSensorData();
+    timer = millis();
+  }
+  
+}
+
+void sendSensorData(){
+
   Temperature = dht.readTemperature(); // Get temperature from sensor
   Humidity = dht.readHumidity();       // Get humidity from sensor
   
-  delay(3000);
-
   // Use WiFiClient class to create TCP connections
   WiFiClient client;
   const char * host = "192.168.0.94"; //Main esp server ip
@@ -78,20 +91,39 @@ void loop() {
   url.replace("dhtTemp_value", String(Temperature));
   url.replace("dhtHum_value", String(Humidity));
   url.replace("id", String(id));
+  Serial.println(url);
 
   // This will send the request to the server
   client.print(String("GET ") + url + " HTTP/1.1\r\n" +
              "Host: " + host + "\r\n" +
              "Connection: close\r\n\r\n");
-  unsigned long timeout = millis();
-  char c = client.read();
-  Serial.print(c);
-  while (client.available() == 0) {
-    if (millis() - timeout > 2000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      return;
-    }
+  client.stop();
+}
+
+void handle_relaySwitch(AsyncWebServerRequest *request){
+  String receivedValue;
+
+  if (request->hasParam("button_reading")){
+    receivedValue = request->arg("button_reading");
   }
+  Serial.println(receivedValue);
+
+  //Yes, bunch of "if", i was too lazy to think about better solution
+  if(receivedValue == "00"){
+    digitalWrite(firstRelayPin, 0);
+  }
+  if(receivedValue == "01"){
+    digitalWrite(firstRelayPin, 1);
+  }
+  if(receivedValue == "10"){
+    digitalWrite(secondRelayPin, 0);
+  }
+  if(receivedValue == "11"){
+    digitalWrite(secondRelayPin, 1);
+  }
+  
+  receivedValue += "\e";
+  
+  request->send(200, "text/plain", receivedValue);
   
 }
--- main_server/main_server.ino
+++ main_server/main_server.ino
@@ -18,8 +18,8 @@ IPAddress gateway(192, 168, 0, 1);
 IPAddress subnet(255, 255, 255, 0);
 
 // Replace with your network credentials
-const char* ssid = "ssid";
-const char* password = "password";
+const char* ssid = "Tobik_Hata";
+const char* password = "P4npYfYS";
 
 //DHT sensor settings
 #define DHTTYPE DHT22   //Type - DHT 22  (AM2302), AM2321
@@ -30,99 +30,17 @@ bool dhtStatus;
 
 unsigned long timer;
 String responseJson;  //JSON object with data for response to the client
-int dataArray[5][5];  //Array witch connected rooms data
+int roomsData[5][7];  //Array which contains rooms data
+bool relayStatus[2];
+
+bool functionCall = false;
+bool currentRelayState[2];
+char currentHost[13] = "192.168.0.10";
+
 
 // Create AsyncWebServer object on port 80
 DynamicJsonBuffer jsonBuffer;
 AsyncWebServer server(80);
-
-//Make a response json data
-void makeResponseJson(){
-
-  responseJson = "";
-  
-  //Check if data is valid
-  if(isnan(temperature) || isnan(humidity)){
-    Serial.println("Incorrect data");
-    temperature = 0;
-    humidity = 0;
-    dhtStatus = -1; //Change status of the sensor to negative
-  }else{
-    dhtStatus = 1;
-  }
-  
-  //Add data from this main room/server
-  StaticJsonBuffer<800> jsonBuffer;
-  JsonObject& root = jsonBuffer.createObject();
-  JsonArray& main_room = root.createNestedArray("main_room");
-  main_room.add(temperature);
-  main_room.add(humidity);
-  main_room.add(dhtStatus);
-
-  //Add data from other rooms/devices to array
-  for(int i = 0; i <= 4; i++){
-    //If device is ON
-    if(dataArray[i][4] == 1){
-      String roomName = "room" + String(i);
-      JsonArray& room = root.createNestedArray(roomName);
-      room.add(dataArray[i][0]);
-      room.add(dataArray[i][1]);
-      room.add(dataArray[i][2]);
-      room.add(dataArray[i][3]);
-    }
-  }
-
-  root.prettyPrintTo(responseJson);
-}
-
-//Send json object on /getData request
-String getData(){
-  makeResponseJson();
-  //Serial.println("Complete json:");
-  //Serial.println(responseJson);
-  return String(responseJson);
-}
-
-//Add or update other rooms data via request from devices
-void handle_receiveData(AsyncWebServerRequest *request){
-   //Receive variables from other esp
-  String sensorValues, deviceName = "room";
-
-  if (request->hasParam("sensor_reading")){
-    sensorValues = request->arg("sensor_reading");
-  }
-
-  //Parse data as json object
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& root = jsonBuffer.parseObject(sensorValues);
-  if (!root.success()) {
-    Serial.println("parseObject() failed");
-    return;
-  }else{
-    deviceName += String(root["esp_num"]);
-  }
-
-  int temp, hum, id;
-  temp = root["dhtTemp_reading"];
-  hum = root["dhtHum_reading"];
-  id = root["esp_num"];
-  
-  //Check if data is valid
-  if(isnan(temp) || isnan(hum)){
-    Serial.println("Incorrect incoming data");
-    temp = 0;
-    hum = 0;
-    dataArray[id][3] = -1;//Change time to negative to let know client that data corrupted
-    dataArray[id][4] = -1;
-  }else{
-    dataArray[id][0] = temp;
-    dataArray[id][1] = hum;
-    dataArray[id][2] = 0; //Time since last connection
-    dataArray[id][3] = id;//Device id
-    dataArray[id][4] = 1; //State of the device ON/OFF/DATA ERROR
-  }
-  
-}
  
 void setup(){
   // Serial port for debugging purposes
@@ -165,13 +83,16 @@ void setup(){
     request->send(SPIFFS, "/style.css", "text/css");
   });
 
-  // Client HTTP request to update data
+  // Send data to the client
   server.on("/getData", HTTP_GET, [](AsyncWebServerRequest *request){
     request->send_P(200, "text/plain", getData().c_str());
   });
 
-  // ESP device HTTP request to receive data
+  // Receive data from other ESP device
   server.on("/receiveData", HTTP_GET, handle_receiveData);
+
+  // Switch checkbox
+  server.on("/checkboxSwitch", HTTP_GET, handle_checkboxSwitch);
 
   // Start server
   server.begin();
@@ -185,11 +106,171 @@ void loop(){
   //Add seconds since the last device connection
   if(millis()-timer >= 1000){
     for(int i = 0; i <= 5; i++){
-      if( dataArray[i][4] != 0){
-        dataArray[i][2] += 1;
+      if( roomsData[i][6] != 0){
+        roomsData[i][2] += 1;
       }
     }
+    //relaySwitch();
     timer = millis();
   }
+  
+  if(functionCall == true){
+    relaySwitch();
+    functionCall = false;
+  }
+  
+}
+
+//Send json object on /getData request
+String getData(){
+  makeResponseJson();
+  //Serial.println("Complete json:");
+  //Serial.println(responseJson);
+  return String(responseJson);
+}
+
+//Make response json object
+void makeResponseJson(){
+
+  responseJson = "";
+  
+  //Check if data is valid
+  if(isnan(temperature) || isnan(humidity)){
+    Serial.println("Incorrect data");
+    temperature = 0;
+    humidity = 0;
+    dhtStatus = -1; //Change status of the sensor to negative
+  }else{
+    dhtStatus = 1;
+  }
+  
+  //Add data from this main room/server
+  StaticJsonBuffer<800> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  JsonArray& main_room = root.createNestedArray("main_room");
+  main_room.add(temperature);
+  main_room.add(humidity);
+  main_room.add(dhtStatus);
+  main_room.add(""); //add the fuckin id for the love of god
+  main_room.add(relayStatus[0]);
+  main_room.add(relayStatus[1]);
+
+  //Add data from other rooms/devices to array
+  for(int i = 0; i <= 4; i++){
+    //If device is ON
+    if(roomsData[i][6] == 1){
+      String roomName = "room" + String(i);
+      JsonArray& room = root.createNestedArray(roomName);
+      room.add(roomsData[i][0]); //Temperature
+      room.add(roomsData[i][1]); //Humidity
+      room.add(roomsData[i][2]); //Time since last receive
+      room.add(roomsData[i][3]); //id
+      room.add(roomsData[i][4]);
+      room.add(roomsData[i][5]);
+    }
+  }
+  root.prettyPrintTo(responseJson);
+}
+
+
+
+//Add or update other rooms data via request from devices
+void handle_receiveData(AsyncWebServerRequest *request){
+  
+  String sensorValues, deviceName = "room";
+
+  if (request->hasParam("sensor_reading")){
+    sensorValues = request->arg("sensor_reading");
+  }
+
+  //Parse data as json object
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(sensorValues);
+  if (!root.success()) {
+    Serial.println("parseObject() failed");
+    return;
+  }else{
+    deviceName += String(root["esp_num"]);
+  }
+
+  int temp, hum, id;
+  temp = root["dhtTemp_reading"];
+  hum = root["dhtHum_reading"];
+  id = root["esp_num"];
+  
+  //Check if data is valid
+  if(isnan(temp) || isnan(hum)){
+    Serial.println("Incorrect incoming data");
+    temp = 100;
+    hum = 0;
+    roomsData[id][3] = -1;//Change time to negative to let know client that data corrupted
+    roomsData[id][4] = -1;
+  }else{
+    roomsData[id][0] = temp;
+    roomsData[id][1] = hum;
+    roomsData[id][2] = 0; //Time since last connection
+    roomsData[id][3] = id;//Device id
+    roomsData[id][6] = 1; //State of the device ON/OFF/DATA ERROR
+  }
+  
+}
+
+void handle_checkboxSwitch(AsyncWebServerRequest *request){
+  
+  int roomNum = (request->arg("roomNum")).toInt();
+  int checkboxNum = (request->arg("checkboxNum")).toInt();
+
+  if(roomNum==0){
+    relayStatus[checkboxNum] = !relayStatus[checkboxNum];
+    
+    request->send(200, "text/plain", String(relayStatus[checkboxNum]));
+  }else{
+    roomsData[roomNum-1][checkboxNum+4] = !roomsData[roomNum-1][checkboxNum+4];
+
+    request->send(200, "text/plain", String(roomsData[roomNum-1][checkboxNum+4]));
+
+    currentRelayState[0] = checkboxNum;
+    currentRelayState[1] = roomsData[roomNum-1][checkboxNum+4];
+
+    
+    char roomIp = roomNum + 48;
+    currentHost[12] = roomIp;
+    functionCall = true;
+    
+  }
+
+}
+
+//Connect to esp device to change relay state
+void relaySwitch(){
+
+  Serial.print("Relay state: ");
+  Serial.println(currentRelayState[0]);
+  WiFiClient client;
+  const char * host = currentHost; //Relay controlling esp ip adress
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.print("Connection to host ");
+    Serial.print(host);
+    Serial.println(" failed");
+    return;
+  }else{
+    Serial.print(host);
+    Serial.println(" Connection established");
+  }
+  // We now create a URI for the request. Something like /data/?sensor_reading=123
+  String url = "/relaySwitch/";
+  url += "?button_reading=";
+  url += currentRelayState[0];
+  url += currentRelayState[1];
+
+  // This will send the request to the server
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+             "Host: " + host + "\r\n" +
+             "Connection: close\r\n\r\n");
+  
+//  String answer = client.readStringUntil('\e');
+//  answer = answer.charAt(answer.length() - 1);
+//  Serial.println(answer);
   
 }
