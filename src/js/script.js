0721f4e9b1b9388d38c60d2705547734f74eef7b
Kirill Mikeylov <keria132@protonmail.com>
2024-05-09 00:57:16 +0300

    feat: more precise location from third party api

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,16 @@
+const getBetterLocation = async () => {
+  const response = await fetch("http://ip-api.com/json/");
+
+  if (response.ok) {
+    const location = await response.json();
+
+    return location.lat + "," + location.lon;
+  }
+}
+
 const getWetherData = async () => {
-  const response = await fetch("http://api.weatherapi.com/v1/current.json?key=44877cb0280f41d1a84130248241604&q=auto:ip&aqi=no");
+  const locationString = await getBetterLocation();
+  const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=44877cb0280f41d1a84130248241604&q=${locationString}&aqi=no`);
 
   if (response.ok) {
     const json = await response.json();
@@ -9,36 +20,36 @@ const getWetherData = async () => {
 }
 
 const displayWeatherWidget = (weather, location) => {
-  const weatherWidget = document.querySelector(".mainPageWidget");
+  const weatherWidget = document.querySelector(".mainWidget");
   const loader = document.querySelector(".loader");
 
   weatherWidget.innerHTML = `
-    <!--<div class="mainPageWidget__icon">⛅</div>-->
-    <div class="mainPageWidget__icon">
+    <!--<div class="mainWidget__icon">⛅</div>-->
+    <div class="mainWidget__icon">
       <img src="${weather.condition.icon}">
     </div>
-    <h2 class="mainPageWidget__weather">${weather.condition.text}</h2>
-    <small class="mainPageWidget__location">${location.region + ", " + location.country}</small>
-    <p class="mainPageWidget__temperature">${weather.temp_c + "°"}</p>
-    <span class="mainPageWidget__details">
+    <h2 class="mainWidget__weather">${weather.condition.text}</h2>
+    <small class="mainWidget__location">${location.region + ", " + location.country}</small>
+    <p class="mainWidget__temperature">${weather.temp_c + "°"}</p>
+    <span class="mainWidget__details">
       <p>${weather.feelslike_c + "°"}</p>
       <small>Sensible</small>
     </span>
-    <span class="mainPageWidget__details">
+    <span class="mainWidget__details">
       <p>${weather.precip_mm + "mm"}</p>
       <small>Precipitation</small>
     </span>
-    <span class="mainPageWidget__details">
+    <span class="mainWidget__details">
       <p>${weather.humidity + "%"}</p>
       <small>Humidity</small>
     </span>
-    <span class="mainPageWidget__details">
+    <span class="mainWidget__details">
       <p>${weather.wind_kph + " km/h"}</p>
       <small>Wind</small>
     </span>
   `
 
-  weatherWidget.classList.toggle("mainPageWidget_loading");
+  weatherWidget.classList.toggle("mainWidget_loading");
   loader.classList.toggle("loader_loaded");
 }
 
